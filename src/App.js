8fbe4045d3da9b1ef2beb37f1282d9ae34e58f0b
Wylie Fisher <me@wyliefisher.com>
2025-05-31 18:52:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Wrap, Back, Anchor, ImageWrap } from "./styled.js";
 const App = ({ children, wrap }) => (
   <Wrap>
     <Back>
-      <Anchor href="http://www.fisherillustration.com" className="illustration" target="_blank">
+      <Anchor href="https://www.fisherillustration.com" className="illustration" target="_blank">
         <ImageWrap className="illustration" />
       </Anchor>
       <Anchor href="https://www.instagram.com/marksfisher3/" className="comix" target="_blank">
