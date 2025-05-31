f931ed8ddb182285197ad40ff01947033b7e52e9
Wylie Fisher <me@wyliefisher.com>
2025-05-31 18:52:42 -0400

    Update index.js

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -5,8 +5,8 @@ import { StyledDiv } from "./styled.js";
 const Page = ({ children }) => (
   <StyledDiv>
     <div class="back">
-      <a href="http://www.fisherillustration.com" class="anchor illustration"><span class="imagewrap"></span></a>
-      <a href="http://www.particle17.com" class="anchor comix"><span class="imagewrap"></span></a>
+      <a href="https://www.fisherillustration.com" class="anchor illustration"><span class="imagewrap"></span></a>
+      <a href="https://www.particle17.com" class="anchor comix"><span class="imagewrap"></span></a>
     </div>
   </StyledDiv>
 );
